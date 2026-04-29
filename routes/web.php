4b920a73c6e3e91ca5eb9f105d8225aa884dc1a1
Ultra <0000727606@senaimgaluno.com.br>
2026-04-29 11:28:43 -0300

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/login', function () {
     return view('entrada');
+});
+
+Route::get('/cadastro', function () {
+    return view('cadastro');
+});
+
+Route::get('/home', function () {
+    return view('index');
 });
