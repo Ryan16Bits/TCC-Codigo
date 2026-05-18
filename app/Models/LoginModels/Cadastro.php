276370485acb6f346a8854bf3190b113307b76af
Ultra <0000727606@senaimgaluno.com.br>
2026-05-17 22:59:02 -0300

    Add files via upload

---
+++ app/Models/LoginModels/Cadastro.php
@@ -0,0 +1,12 @@
+<?php
+ 
+namespace App\Models;
+ 
+use Illuminate\Database\Eloquent\Model;
+ 
+class Cadastro extends Model
+{
+    // Campos que podem ser salvos no banco
+    protected $fillable = ['email', 'senha'];
+}
+ 
