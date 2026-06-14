9c75e4fe2b77449288f37e984f61a158fa4f4b12
Ultra <0000727606@senaimgaluno.com.br>
2026-06-14 00:29:49 -0300

    Add test route returning 'Funcionando!'

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsuarioController\WebsiteController;
 use App\Http\Controllers\AdminController\AdminController;
 
+Route::get('/', function() {
+    return 'Funcionando!';
+});
+
+// Teste
+/*
 Route::get('/', [WebsiteController::class, 'login']);
 Route::post('/', [WebsiteController::class, 'login'])->name('login.post');
 Route::get('/cadastro', [WebsiteController::class, 'cadadstro']);
@@ -36,3 +42,5 @@ Route::get('/esqueciSenha', [WebsiteController::class, 'esqueciSenha']);
 Route::get('/esqueciSenha2', [WebsiteController::class, 'esqueciSenha2']);
 Route::get('/notificacoesLista', [WebsiteController::class, 'notificacoesLista']);
 Route::get('/homeCuidador', [WebsiteController::class, 'homeCuidador']);
+// Teste
+*/
