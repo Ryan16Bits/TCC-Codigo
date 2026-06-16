98b147435ac02e9b18c705c6d2115b9327d54182
Ultra <0000727606@senaimgaluno.com.br>
2026-06-16 08:55:24 -0300

    Update form action for login submission

--- resources/views/login/entrada.blade.php
+++ resources/views/login/entrada.blade.php
@@ -19,7 +19,7 @@
 
                     <div class="card-body p-4 bg-custom-card">
 
-                        <form class="list" method="post" action="">
+                        <form class="list" method="post" action="{{ route('login.post') }}">
 
                             <!-- Email -->
                             <div class="mb-3">
