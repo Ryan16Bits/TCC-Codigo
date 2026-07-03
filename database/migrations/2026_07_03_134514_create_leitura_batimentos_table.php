07baed19e8c796e86644b31c61fc90c3e904206d
Ultra <0000727606@senaimgaluno.com.br>
2026-07-03 13:08:31 -0300

    Add files via upload

---
+++ database/migrations/2026_07_03_134514_create_leitura_batimentos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('leitura_batimentos', function (Blueprint $table) {
+            $table->id('idBatimento');
+            $table->unsignedBigInteger('idDispositivo');
+            $table->unsignedBigInteger('idLeitura');
+            $table->doublePrecision('bpm');
+            $table->timestamp('detectadoEm');
+            $table->timestamps();
+            $table->foreign('idDispositivo')->references('idDispositivo')->on('dispositivos')->onDelete('cascade');
+            $table->foreign('idLeitura')->references('idLeitura')->on('leitura_sensores')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('leitura_batimentos');
+    }
+};
